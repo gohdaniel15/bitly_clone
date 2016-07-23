7f80bd3135e6b7269e9dd8972baaf51ec4579706
gohdaniel15 <gohdaniel15@gmail.com>
2016-07-23 14:57:28 +0800

    linked images to img folder. cleaned up css file

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,15 +1,10 @@
 class Url < ActiveRecord::Base
 	# This is Sinatra! Remember to create a migration!
-
-	validates :long_url, uniqueness: true, presence: true, format: {
-		with: /http.?:\/\/.+/,
-		message: "Invalid URL"
-	}
+	validates :long_url, uniqueness: true, presence: true
 
 	def shorten
 		rnumbers = []
 		8.times { rnumbers << rand(1..9) }
-		self.short_url = rnumbers.join.to_i.to_s(36)
+		self.short_url = rnumbers.join.to_i.to_s(36) # Can also use SecureRandom.hex(5) to generate random digits.
 	end
 end
-
